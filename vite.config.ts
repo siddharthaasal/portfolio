fa1dcebc29b7731154379b15e13e45fc6e5ee5de
siddharthaasal <siddharthaasal@gmail.com>
2025-07-07 18:15:47 +0530

    perf(build): enable manual chunking for react, react-dom, framer-motion

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      filename: 'dist/report.html', // save loc
+      open: true,                    // auto-open in browser after build
+      gzipSize: true,
+      brotliSize: true
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom')) return 'react-dom';
+            if (id.includes('react')) return 'react';
+            if (id.includes('framer-motion')) return 'framer';
+            if (id.includes('react-icons')) return 'icons';
+          }
+        },
+      },
     },
   },
 })
